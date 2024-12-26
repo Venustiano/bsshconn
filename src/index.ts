8189fd05176a25d8ebb9d1040adc1a9483bb63bc
Venus Soancatl Aguilar <vsoancatl@mw171083.med.rug.nl>
2024-12-26 21:35:00 +0100

    Show ssh command in palette

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import {
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+
 /**
  * Initialization data for the jupyterlab_bsshconn extension.
  */
@@ -13,9 +17,45 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'A JupyterLab extension for ssh connections.',
   autoStart: true,
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette, settingRegistry: ISettingRegistry | null) => {
     console.log('JupyterLab extension jupyterlab_bsshconn is activated!');
+    console.log('ICommandPalette:', palette);
 
+    // Define a widget creator function,
+    // then call it to make a new widget
+    const newWidget = () => {
+      // Create a blank content widget inside of a MainAreaWidget
+      const content = new Widget();
+      const widget = new MainAreaWidget({ content });
+      widget.id = 'bsshconn-jupyterlab';
+      widget.title.label = 'SSH connection';
+      widget.title.closable = true;
+      return widget;
+    }
+    let widget = newWidget();
+
+    // Add an application command
+    const command: string = 'bsshconn:open';
+    app.commands.addCommand(command, {
+      label: 'SSH connection',
+      execute: () => {
+        // Regenerate the widget if disposed
+        if (widget.isDisposed) {
+          widget = newWidget();
+        }
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
+      
     if (settingRegistry) {
       settingRegistry
         .load(plugin.id)
